b5f73189624e5eea5557fbc68590b58716173a3d
Fru(Kang Dongwook) <kangfru@gmail.com>
2025-05-22 17:22:32 +0900

    이용하지 않는 import 수정

--- src/main/kotlin/com/kangfru/common/lock/DistributedLockAspect.kt
+++ src/main/kotlin/com/kangfru/common/lock/DistributedLockAspect.kt
@@ -4,7 +4,6 @@ import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.reflect.MethodSignature
-import org.hibernate.exception.LockAcquisitionException
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.expression.spel.standard.SpelExpressionParser
 import org.springframework.expression.spel.support.StandardEvaluationContext
